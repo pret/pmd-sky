c70d1d2fef12dc3ef922a682cd1bdf81e7215c6b
marius david <marius@mariusdavid.fr>
2023-09-04 18:27:58 +0200

    Finishing decompilation of pack-related functions

--- include/main_pack.h
+++ include/main_pack.h
@@ -3,6 +3,12 @@
 
 #include <files/pack.h>
 
+void OpenAllPackFiles(void);
+u32 GetFileLengthInPackWithPackNb(enum pack_file_id pack_id, u32 file_index);
+u32 LoadFileInPackWithPackId(enum pack_file_id pack_id, u32 file_index, void* output_buffer);
+void AllocAndLoadFileInPack(enum pack_file_id pack_id, u32 file_index, struct pack_alloc_and_load_result *output, u32 malloc_flags);
+void OpenPackFile(struct pack_file_opened *pack_file,char *file_path);
 u32 GetFileLengthInPack(struct pack_file_opened* pack_file, u32 file_index);
+u32 LoadFileInPack(struct pack_file_opened *pack_file,void *output_buffer,u32 file_index);
 
 #endif //PMDSKY_MAIN_PACK_H
