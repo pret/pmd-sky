d0b6396c025eeb88f420bad7edc5c4a361e1d877
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2024-06-19 23:01:35 -0500

    Matched overlay 14

--- asm/overlay_14.s
+++ asm/overlay_14.s
@@ -737,7 +737,12 @@ ov14_0238ABA0: ; 0x0238ABA0
 	mov r5, r0
 	mov r0, r1, asr #0x10
 	bl GetSentryDutyGamePoints
+#ifdef JAPAN
+	add r1, r4, #0x49
+	add r1, r1, #0x3d00
+#else
 	add r1, r4, #0x6e0
+#endif
 	mov r1, r1, lsl #0x10
 	str r0, [sp, #0x28]
 	mov r0, r1, lsr #0x10
@@ -788,8 +793,13 @@ ov14_0238AC40: ; 0x0238AC40
 	ldr r2, [r0, #0x880]
 	ldr r3, [r0, #0x888]
 	mov r0, r2, lsl #2
+#ifdef JAPAN
+	add r0, r0, #0xd60
+	add r0, r0, #0x3000
+#else
 	add r0, r0, #0xf7
 	add r0, r0, #0x600
+#endif
 	add r0, r3, r0
 	str r0, [ip, #0x114]
 	ldr r0, [r1]
@@ -839,6 +849,11 @@ ov14_0238ACEC: ; 0x0238ACEC
 
 	arm_func_start ov14_0238AD04
 ov14_0238AD04: ; 0x0238AD04
+#ifdef JAPAN
+#define OV14_0238AD04_OFFSET -2
+#else
+#define OV14_0238AD04_OFFSET 0
+#endif
 	stmdb sp!, {r4, lr}
 	sub sp, sp, #0x18
 	mov r4, r0
@@ -855,10 +870,10 @@ ov14_0238AD04: ; 0x0238AD04
 	ldrsh r1, [r0, #0xa2]
 	add r0, sp, #0
 	bl GetNameRaw
-	mov r1, #2
+	mov r1, #2 + OV14_0238AD04_OFFSET
 	add r3, sp, #0
 	mov r0, r4
-	sub r2, r1, #3
+	sub r2, r1, #3 + OV14_0238AD04_OFFSET
 	bl DrawTextInWindow
 	ldr r1, _0238ADF8 ; =SENTRY_DUTY_PTR
 	add r0, sp, #0
@@ -866,9 +881,9 @@ ov14_0238AD04: ; 0x0238AD04
 	add r1, r1, #0x3800
 	ldrsh r1, [r1, #0xa4]
 	bl GetNameRaw
-	mov r1, #0x7a
+	mov r1, #0x7a + OV14_0238AD04_OFFSET
 	mov r0, r4
-	sub r2, r1, #0x7b
+	sub r2, r1, #0x7b + OV14_0238AD04_OFFSET
 	add r3, sp, #0
 	bl DrawTextInWindow
 _0238AD84:
@@ -881,10 +896,10 @@ _0238AD84:
 	ldrsh r1, [r0, #0xa6]
 	add r0, sp, #0
 	bl GetNameRaw
-	mov r1, #2
+	mov r1, #2 + OV14_0238AD04_OFFSET
 	add r3, sp, #0
 	mov r0, r4
-	sub r2, r1, #3
+	sub r2, r1, #3 + OV14_0238AD04_OFFSET
 	bl DrawTextInWindow
 	ldr r1, _0238ADF8 ; =SENTRY_DUTY_PTR
 	add r0, sp, #0
@@ -892,9 +907,9 @@ _0238AD84:
 	add r1, r1, #0x3800
 	ldrsh r1, [r1, #0xa8]
 	bl GetNameRaw
-	mov r1, #0x7a
+	mov r1, #0x7a + OV14_0238AD04_OFFSET
 	mov r0, r4
-	sub r2, r1, #0x7b
+	sub r2, r1, #0x7b + OV14_0238AD04_OFFSET
 	add r3, sp, #0
 	bl DrawTextInWindow
 _0238ADE8:
@@ -1672,6 +1687,20 @@ _0238B8BC:
 	mvn r0, #1
 	cmp r2, r0
 	bne _0238B96C
+#ifdef JAPAN
+	mov r0, #0
+	str r0, [r3, #0xc]
+	ldr r0, [r1]
+	ldr r3, _0238D698_JP ; =0x00003D47
+	mov r2, #0x10
+	strh r3, [r0, #0x14]
+	ldr r0, [r1]
+	mov r4, #5
+	str r2, [r0, #0x18]
+	ldr r2, [r1]
+	ldr r1, _0238C134 ; =0x04080800
+	ldr r0, [r2, #0x11c]
+#else
 	mov r2, #0
 	str r2, [r3, #0xc]
 	ldr r2, [r1]
@@ -1684,6 +1713,7 @@ _0238B8BC:
 	mov r4, #5
 	ldr r0, [r2, #0x11c]
 	ldr r1, _0238C134 ; =0x04080800
+#endif
 	cmp r0, #2
 	str r4, [sp]
 	beq _0238B94C
@@ -1773,15 +1803,25 @@ _0238B9E0: ; jump table
 	b _0238BD6C ; case 33
 _0238BA68:
 	ldr r1, _0238C14C ; =0x00000142
+#ifdef JAPAN
+	ldr r4, _0238C144 ; =0x00003018
+	str r1, [r0, #0xf0]
+	add r5, r1, #0x3c00
+#else
 	ldr r5, _0238C150 ; =0x000006D9
 	str r1, [r0, #0xf0]
 	ldr r4, _0238C144 ; =0x00003018
+#endif
 	b _0238BD6C
 _0238BA7C:
 	ldr r1, _0238C14C ; =0x00000142
 	ldr r5, _0238C154 ; =0x000006D8
 	str r1, [r0, #0xf0]
+#ifdef JAPAN
+	ldr r4, _0238C144 ; =0x00003018
+#else
 	add r4, r5, #0x2940
+#endif
 	b _0238BD6C
 _0238BA90:
 	ldr r1, _0238C14C ; =0x00000142
@@ -1865,11 +1905,19 @@ _0238BB7C:
 	str r1, [r0, #0x8ac]
 	b _0238BBD8
 _0238BBAC:
+#ifdef JAPAN
+	ldr r3, _0238C14C ; =0x00000142
+	ldr r1, _0238C180 ; =0x00002C07
+	str r3, [r0, #0xf0]
+	ldr r0, [r2]
+	ldr r5, _0238D6E4 ; =0x00003D5B
+#else
 	ldr r5, _0238C17C ; =0x000006F2
 	ldr r1, _0238C180 ; =0x00002C07
 	sub r3, r5, #0x5b0
 	str r3, [r0, #0xf0]
 	ldr r0, [r2]
+#endif
 	str r1, [r0, #0xc8]
 	ldr r0, [r2]
 	add r0, r0, #0x3000
@@ -1917,7 +1965,11 @@ _0238BC50:
 	cmp r1, #0
 	bne _0238BC80
 	ldr r1, _0238C14C ; =0x00000142
+#ifdef JAPAN
+	ldr r5, _0238D6F0_JP ; =0x00003D59
+#else
 	mov r5, #0x6f0
+#endif
 	str r1, [r0, #0xf0]
 	ldr r0, [r2]
 	add r0, r0, #0x3000
@@ -2253,8 +2305,16 @@ _0238C124:
 	add sp, sp, #8
 	ldmia sp!, {r3, r4, r5, pc}
 	.align 2, 0
+#ifdef JAPAN
+#define SENTRY_RUN_STATE_OFFSET 0x3669
+#else
+#define SENTRY_RUN_STATE_OFFSET 0
+#endif
 _0238C12C: .word SENTRY_DUTY_PTR
 _0238C130: .word SENTRY_DUTY_STATE_HANDLER_TABLE
+#ifdef JAPAN
+_0238D698_JP: .word 0x00003D47
+#endif
 _0238C134: .word 0x04080800
 _0238C138: .word ov14_0238D9C8
 _0238C13C: .word ov14_0238ABA0
@@ -2262,27 +2322,37 @@ _0238C140: .word ov14_0238D9D8
 _0238C144: .word 0x00003018
 _0238C148: .word ov14_0238D9B8
 _0238C14C: .word 0x00000142
+#ifndef JAPAN
 _0238C150: .word 0x000006D9
-_0238C154: .word 0x000006D8
-_0238C158: .word 0x000006DA
-_0238C15C: .word 0x000006DB
+#endif
+_0238C154: .word 0x000006D8 + SENTRY_RUN_STATE_OFFSET
+_0238C158: .word 0x000006DA + SENTRY_RUN_STATE_OFFSET
+_0238C15C: .word 0x000006DB + SENTRY_RUN_STATE_OFFSET
 _0238C160: .word 0x00003028
-_0238C164: .word 0x000006EB
-_0238C168: .word 0x000006DC
-_0238C16C: .word 0x000006EE
-_0238C170: .word 0x000006EF
+_0238C164: .word 0x000006EB + SENTRY_RUN_STATE_OFFSET
+_0238C168: .word 0x000006DC + SENTRY_RUN_STATE_OFFSET
+_0238C16C: .word 0x000006EE + SENTRY_RUN_STATE_OFFSET
+_0238C170: .word 0x000006EF + SENTRY_RUN_STATE_OFFSET
 _0238C174: .word 0x00002311
-_0238C178: .word 0x000006F1
+_0238C178: .word 0x000006F1 + SENTRY_RUN_STATE_OFFSET
+#ifdef JAPAN
+_0238C180: .word 0x00002C07
+_0238D6E4: .word 0x00003D5B
+#else
 _0238C17C: .word 0x000006F2
 _0238C180: .word 0x00002C07
-_0238C184: .word 0x000006EC
-_0238C188: .word 0x000006ED
+#endif
+_0238C184: .word 0x000006EC + SENTRY_RUN_STATE_OFFSET
+_0238C188: .word 0x000006ED + SENTRY_RUN_STATE_OFFSET
+#ifdef JAPAN
+_0238D6F0_JP: .word 0x00003D59
+#endif
 _0238C18C: .word 0x00002C09
-_0238C190: .word 0x000006F3
-_0238C194: .word 0x000006F4
+_0238C190: .word 0x000006F3 + SENTRY_RUN_STATE_OFFSET
+_0238C194: .word 0x000006F4 + SENTRY_RUN_STATE_OFFSET
 _0238C198: .word 0x000001E3
-_0238C19C: .word 0x000006F5
-_0238C1A0: .word 0x000006F6
+_0238C19C: .word 0x000006F5 + SENTRY_RUN_STATE_OFFSET
+_0238C1A0: .word 0x000006F6 + SENTRY_RUN_STATE_OFFSET
 _0238C1A4: .word ov14_0238D9F8
 _0238C1A8: .word ov14_0238ACEC
 _0238C1AC: .word ov14_0238D988
@@ -4162,11 +4232,21 @@ ov14_0238D970:
 	.global ov14_0238D988
 ov14_0238D988:
 	.byte 0x00, 0x00, 0x00, 0x00
-	.byte 0x02, 0x07, 0x1A, 0x02, 0x00, 0xFB, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+#ifdef JAPAN
+	.byte 0x03, 0x07, 0x18, 0x02
+#else
+	.byte 0x02, 0x07, 0x1A, 0x02
+#endif
+	.byte 0x00, 0xFB, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 	.global ov14_0238D998
 ov14_0238D998:
 	.byte 0x00, 0x00, 0x00, 0x00
-	.byte 0x02, 0x10, 0x1A, 0x02, 0x00, 0xFB, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+#ifdef JAPAN
+	.byte 0x03, 0x10, 0x18, 0x02
+#else
+	.byte 0x02, 0x10, 0x1A, 0x02
+#endif
+	.byte 0x00, 0xFB, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 	.global ov14_0238D9A8
 ov14_0238D9A8:
 	.byte 0x00, 0x00, 0x00, 0x00
@@ -4193,8 +4273,15 @@ ov14_0238D9F8:
 	.byte 0x02, 0x02, 0x1D, 0x05, 0x01, 0xFB, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 	.global ov14_0238DA08
 ov14_0238DA08:
-	.byte 0xD6, 0x06, 0x00, 0x00
-	.byte 0x08, 0x00, 0x00, 0x00, 0xD7, 0x06, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+#ifdef JAPAN
+#define OV14_DATA_OFFSET 0x3669
+#else
+#define OV14_DATA_OFFSET 0
+#endif
+	.word 0x6D6 + OV14_DATA_OFFSET
+	.byte 0x08, 0x00, 0x00, 0x00
+	.word 0x6D7 + OV14_DATA_OFFSET
+	.byte 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 	.byte 0x08, 0x00, 0x00, 0x00
 	.global ov14_0238DA20
 ov14_0238DA20:
@@ -4218,10 +4305,21 @@ ov14_0238DA80:
 	.byte 0x0A, 0x00, 0x00, 0x00
 	.global SENTRY_DEBUG_MENU_ITEMS
 SENTRY_DEBUG_MENU_ITEMS:
-	.byte 0xCE, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xCF, 0x06, 0x00, 0x00
-	.byte 0x01, 0x00, 0x00, 0x00, 0xD0, 0x06, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0xD1, 0x06, 0x00, 0x00
-	.byte 0x03, 0x00, 0x00, 0x00, 0xD2, 0x06, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0xD3, 0x06, 0x00, 0x00
-	.byte 0x05, 0x00, 0x00, 0x00, 0xD4, 0x06, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0xD5, 0x06, 0x00, 0x00
+	.word 0x6CE + OV14_DATA_OFFSET
+	.byte 0x00, 0x00, 0x00, 0x00
+	.word 0x6CF + OV14_DATA_OFFSET
+	.byte 0x01, 0x00, 0x00, 0x00
+	.word 0x6D0 + OV14_DATA_OFFSET
+	.byte 0x02, 0x00, 0x00, 0x00
+	.word 0x6D1 + OV14_DATA_OFFSET
+	.byte 0x03, 0x00, 0x00, 0x00
+	.word 0x6D2 + OV14_DATA_OFFSET
+	.byte 0x04, 0x00, 0x00, 0x00
+	.word 0x6D3 + OV14_DATA_OFFSET
+	.byte 0x05, 0x00, 0x00, 0x00
+	.word 0x6D4 + OV14_DATA_OFFSET
+	.byte 0x06, 0x00, 0x00, 0x00
+	.word 0x6D5 + OV14_DATA_OFFSET
 	.byte 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 	.global ov14_0238DAE8
 ov14_0238DAE8:
