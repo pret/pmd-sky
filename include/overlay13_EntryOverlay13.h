8f640fdc6e5e6e605648da3be37c19f8cb54cd40
Seth Barberee <seth.barberee@gmail.com>
2024-07-07 13:01:33 -0700

    start overlay 13

---
+++ include/overlay13_EntryOverlay13.h
@@ -0,0 +1,8 @@
+#ifndef PMDSKY_OVERLAY_13_H
+#define  PMDSKY_OVERLAY_13_H
+
+void EntryOverlay13(void);
+void ExitOverlay13(void);
+u32 Overlay13SwitchFunctionNa238A1C8(void);
+
+#endif // PMDSKY_OVERLAY_13
