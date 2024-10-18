d6748a67e538a33ae09cc3b3d597c23c1afb1eca
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2024-10-18 01:21:12 -0400

    Decomped GetMoneyCarried
    Decomped by Seti

---
+++ include/main_0200ECFC.h
@@ -0,0 +1,13 @@
+#ifndef PMDSKY_MAIN_0200ECFC_H
+#define PMDSKY_MAIN_0200ECFC_H
+
+typedef struct {
+    u8 fill1[0x388];
+    u8 unk388;
+    u8 fill2[0x1009];
+    u32 maybeMoney[]; // Unknown size, but indexed with an u8
+} bag_items;
+
+u32 GetMoneyCarried(void);
+
+#endif //PMDSKY_MAIN_0200ECFC_H
