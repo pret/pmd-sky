08323f7f5debfb13e19d668efbfa4d287fb34d2a
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2024-10-18 01:28:01 -0400

    Decomped ov15_0238AE6C and ov15_0238AE88
    Decomped by Seti

---
+++ src/overlay_15_0238AE6C.c
@@ -0,0 +1,17 @@
+#include "overlay_15_0238AE6C.h"
+
+extern unkStruct_ov15_0238AE6C* OVERLAY15_UNKNOWN_POINTER__NA_238B180;
+
+extern void ov15_0238A140(void);
+extern void ov15_0238AD78(u32);
+
+void ov15_0238AE6C(void)
+{
+    ov15_0238AD78(OVERLAY15_UNKNOWN_POINTER__NA_238B180->unkD8);
+}
+
+s32 ov15_0238AE88(void)
+{
+    ov15_0238A140();
+    return 1;
+}
