d2374b4ed236590c5d8f170eb26a846ea4e4a305
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-03-25 00:06:37 -0400

    Removed GetTile externs

--- src/dungeon_range.c
+++ src/dungeon_range.c
@@ -1,11 +1,11 @@
 #include "dungeon_range.h"
 #include "dg_camera.h"
 #include "dungeon.h"
+#include "dungeon_map_access.h"
 #include "map.h"
 
 extern struct dungeon *DUNGEON_PTR[];
 
-extern struct tile* GetTile(int x, int y);
 extern s32 abs(s32 value);
 
 bool8 IsPositionActuallyInSight(struct position *origin, struct position *target, bool8 user_has_dropeye)
