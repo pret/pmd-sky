dfef6a94b7e79d955cd045ffc0d9f0062e06384c
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-03-25 23:39:37 -0400

    Decomped abs

---
+++ include/main_0208655C.h
@@ -0,0 +1,7 @@
+#ifndef PMDSKY_MAIN_0208655C_H
+#define PMDSKY_MAIN_0208655C_H
+
+// Takes the absolute value of an integer.
+s32 abs(s32 value);
+
+#endif //PMDSKY_MAIN_0208655C_H
---
+++ src/main_0208655C.c
@@ -0,0 +1,6 @@
+#include "main_0208655C.h"
+
+s32 abs(s32 value)
+{
+    return value < 0 ? -value : value;
+}
