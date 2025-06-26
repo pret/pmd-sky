c45b7dda7a3a8ac713164064f096739b2a419fbe
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-06-25 21:45:27 -0400

    Decomped IsUsableWhileTaunted

---
+++ include/moves_3.h
@@ -0,0 +1,10 @@
+#ifndef PMDSKY_MOVES_3_H
+#define PMDSKY_MOVES_3_H
+
+#include "move.h"
+#include "util.h"
+
+// Returns true if the given move can be used while the user is affected by Taunt, or false if the move cannot be used while taunted.
+bool8 IsUsableWhileTaunted(struct move *move);
+
+#endif //PMDSKY_MOVES_3_H
