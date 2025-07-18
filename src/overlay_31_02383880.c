7b6a5099a56cd1a9186f58ea22338e00179759c7
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-07-17 21:49:51 -0400

    Decomped ov31_02383880
    Decomped by anonymous user (https://decomp.me/scratch/vv7Lu)

---
+++ src/overlay_31_02383880.c
@@ -0,0 +1,20 @@
+#include "overlay_31_02383880.h"
+#include "dungeon.h"
+#include "overlay_31_02383478.h"
+
+extern struct dungeon* DUNGEON_PTR[];
+
+void ov31_02383880(u8* arg1)
+{
+    u8 count;
+    arg1[0] = 0;
+    count = arg1[0] + 1;
+    for (s32 i=0; i < MAX_TEAM_MEMBERS; i++) {
+        u8 test = EntityIsValid__02383658(DUNGEON_PTR[0]->monster_slots.party_members[i]);
+        if (!test) {
+            arg1[i+1] = 0;
+        } else {
+            arg1[i+1] = count++;
+        }
+    }
+}
