97ad668aecb621a28418d93bd23409cf1da76eef
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-07-17 22:31:32 -0400

    Decomped MirrorMoveIsActive

--- include/overlay_29_023197A8.h
+++ include/overlay_29_023197A8.h
@@ -3,6 +3,9 @@
 
 #include "dungeon_mode.h"
 
+// Checks if the monster is under the effect of Mirror Move.
+// Returns 1 if the effects is a status, 2 if it comes from an exclusive item, 0 otherwise.
+u8 MirrorMoveIsActive(struct entity *entity);
 // Checks if a monster is a team member under the effects of a certain exclusive item effect.
 bool8 ExclusiveItemEffectIsActive__023197A8(struct entity *entity, enum exclusive_item_effect_id effect_id);
 
--- src/overlay_29_023197A8.c
+++ src/overlay_29_023197A8.c
@@ -1,6 +1,24 @@
 #include "overlay_29_023197A8.h"
+#include "dg_random.h"
+#include "dungeon_parameters_1.h"
 #include "dungeon_util_static.h"
 #include "exclusive_item.h"
+#include "move_effects.h"
+#include "overlay_29_02318E4C.h"
+
+u8 MirrorMoveIsActive(struct entity *entity)
+{
+    if (EntityIsValid__02318E4C(entity))
+    {
+        if (GetEntInfo(entity)->reflect_class_status.reflect == STATUS_REFLECT_MIRROR_MOVE)
+            return EFFECT_ACTIVE_FROM_STATUS;
+
+        if (ExclusiveItemEffectIsActive__023197A8(entity, EXCLUSIVE_EFF_MAY_BOUNCE_MOVES) && DungeonRandOutcome__022EAB20(CHATOT_SCARF_BOUNCE_CHANCE))
+            return EFFECT_ACTIVE_FROM_EXCLUSIVE_ITEM;
+    }
+
+    return EFFECT_INACTIVE;
+}
 
 bool8 ExclusiveItemEffectIsActive__023197A8(struct entity *entity, enum exclusive_item_effect_id effect_id)
 {
