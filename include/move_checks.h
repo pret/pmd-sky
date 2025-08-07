bf5943365359f6f57e8d03828e5c0b9bb0fefde9
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-08-07 19:42:07 -0400

    Moved StatusCheckerCheck nonmatching to C file

--- include/move_checks.h
+++ include/move_checks.h
@@ -3,6 +3,10 @@
 
 #include "dungeon_mode.h"
 
+// Determines if using a given move against its intended targets would be redundant because all of them already have the effect caused by said move. Used for moves that affect the user or allies.
+// attacker: Pointer to the entity that is considering using the move
+// move: Move pointer
+// return: True if it makes sense to use the move, false if it would be redundant given the effects it causes and the effects that all the targets already have.
 bool8 StatusCheckerCheck(struct entity *attacker, struct move *move);
 // Checks if an entity pointer points to a valid entity (not entity type 0, which represents no entity).
 bool8 EntityIsValid__02333FAC(struct entity *entity);
