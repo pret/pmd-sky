d5f4a70b28ffee54a3c66cb2e622a0e748442b13
Seth Barberee <seth.barberee@gmail.com>
2025-08-22 16:13:50 -0700

    initial decomp work
    decomp more item funcs
    
    new file boundary and more decomp
    
    address review comments
    
    Match IsValidTargetItem thanks to @taxicat1

--- include/main_02054DC4.h
+++ include/main_02054DC4.h
@@ -4,5 +4,9 @@
 #include "util.h"
 
 bool32 DexNumbersEqual(s16 monster1, s16 monster2);
+u8 GendersEqual(s16 monster1, s16 monster2);
+u8 GendersEqualNotGenderless(s16 monster1, s16 monster2);
+u8 GendersNotEqualNotGenderless(s16 monster1, s16 monster2);
+u8 sub_02054F18(s32 _arg0);
 
 #endif //PMDSKY_MAIN_02054DC4_H
