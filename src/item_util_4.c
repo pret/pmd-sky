2b656b67a8bc2029f7a4c894b2c1c0e84cde749b
Seth Barberee <seth.barberee@gmail.com>
2025-08-27 21:25:06 -0700

    More item decomp

--- src/item_util_4.c
+++ src/item_util_4.c
@@ -6,6 +6,9 @@
 
 extern s32 GetItemSellPrice(s16);
 extern s32 GetItemBuyPrice(s16);
+extern s32 GetThrownItemQuantityLimit(s16, s32);
+extern s16 RandRangeSafe(s32, s32);
+extern s16 _020A18BC[10];
 
 BOOL IsLosableItem(struct item* item)
 {
@@ -48,14 +51,13 @@ bool8 IsShoppableItem(s16 item_id)
     return 1;
 }
 
-extern s16 _020A18BC[10];
-
-bool8 IsValidTargetItem(s16 item_id) {
+bool8 IsValidTargetItem(s16 item_id)
+{
     s32 var_ip;
 
-    if (item_id >= 0x16C) return TRUE;
+    if (item_id >= ITEM_GORGEOUS_BOX_1) return TRUE;
 
-    for (var_ip = 0; _020A18BC[var_ip] != 0x578; var_ip++)
+    for (var_ip = 0; _020A18BC[var_ip] != NUM_ITEM_IDS; var_ip++)
     {
         if (item_id == _020A18BC[var_ip]) {
             return TRUE;
@@ -63,4 +65,81 @@ bool8 IsValidTargetItem(s16 item_id) {
     } 
     
     return FALSE;
+}
+
+bool8 IsItemUsableNow(struct item* item)
+{
+    s16 item_id;
+
+    if (item->flags & ITEM_FLAG_IN_SHOP) {
+        if (GetItemCategory(item->id) != CATEGORY_TMS_HMS) {
+            item_id = item->id;
+            if ((item_id != ITEM_SITRUS_BERRY) && 
+                (item_id != ITEM_LIFE_SEED) && 
+                (item_id != ITEM_GINSENG) && 
+                (item_id != ITEM_JOY_SEED) && 
+                (item_id != ITEM_GOLDEN_SEED) && 
+                (item_id != ITEM_PROTEIN) && 
+                (item_id != ITEM_CALCIUM) && 
+                (item_id != ITEM_IRON) && 
+                (item_id != ITEM_NECTAR) && 
+                (item_id != ITEM_ZINC)) {
+                if ((u16) (s16) (item_id - 0x77) > 0x11) {
+                    goto _ret;
+                }
+            }
+        }
+        return FALSE;
+    }
+_ret:
+    return TRUE;
+}
+
+bool8 IsTicketItem(s16 item_id)
+{
+    s32 arg0 = (u16)(s16)(item_id - ITEM_PRIZE_TICKET);
+
+    return ((arg0) <= 3U);
+}
+
+void InitItem(struct item* item, s16 id, s16 quantity, bool8 isSticky)
+{
+    s32 category;
+    s32 to;
+
+    if (id != 0) {
+        item->flags = ITEM_FLAG_EXISTS;
+        item->id = id;
+        item->held_by = 0;
+        if (IsThrownItem(id)) {
+            to = GetThrownItemQuantityLimit(id, 1);
+            item->quantity = RandRangeSafe(GetThrownItemQuantityLimit(id, 0), to);
+        } else {
+            category = GetItemCategory(id);
+            if (category == CATEGORY_POKE) {
+                item->quantity = 1;
+            } else if ((u8) (category + 0xF4) <= 2) {
+                item->quantity = quantity;
+            } else {
+                item->quantity = 0;
+            }
+        }
+        if (isSticky != 0) {
+            item->flags |= ITEM_FLAG_STICKY;
+        }
+        return;
+    }
+    item->flags = 0;
+    item->id = id;
+    item->quantity = 0;
+}
+
+void InitStandardItem(struct item* item, s16 id, bool8 isSticky)
+{
+   InitItem(item, id, 0, isSticky);
+}
+
+void sub_0200CF6C(struct item* item, s16 id, s16 quantity, bool8 isSticky)
+{
+    InitItem(item, id, quantity, isSticky);
 }
