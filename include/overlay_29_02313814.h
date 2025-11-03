dcb463cd541b762bd74eee66035a5d6235789baf
DizzyEggg <jajkodizzy@wp.pl>
2025-11-03 11:06:57 +0100

    remove move orb effects file

--- include/overlay_29_02313814.h
+++ include/overlay_29_02313814.h
@@ -1,6 +1,14 @@
 #ifndef PMDSKY_OVERLAY_29_02313814_H
 #define PMDSKY_OVERLAY_29_02313814_H
 
+#include "dungeon_mode.h"
+
+enum flash_fire_status {
+    FLASH_FIRE_STATUS_NONE = 0,
+    FLASH_FIRE_STATUS_MAXED = 1,
+    FLASH_FIRE_STATUS_NOT_MAXED = 2
+};
+
 #define STAT_INDEX_PHYSICAL 0 // Atk, Def
 #define STAT_INDEX_SPECIAL 1 // Sp Atk, Sp Def
 #define STAT_INDEX_ACCURACY STAT_INDEX_PHYSICAL
@@ -15,5 +23,8 @@ void LowerOffensiveStat(struct entity *user, struct entity *target, struct StatI
 void LowerDefensiveStat(struct entity *user, struct entity *target, struct StatIndex stat, s32 increment, bool8 checkProtected, bool8 logMsgProtected);
 void BoostOffensiveStat(struct entity *user, struct entity *target, struct StatIndex stat, s32 nStagesRaw);
 void BoostDefensiveStat(struct entity *user, struct entity *target, struct StatIndex stat, s32 nStagesRaw);
+// Checks whether Flash Fire should activate, assuming the defender is being hit by a Fire-type move.
+// This checks that the defender is valid and Flash Fire is active, and that Normalize isn't active on the attacker.
+enum flash_fire_status FlashFireShouldActivate(struct entity *attacker, struct entity *defender);
 
 #endif //PMDSKY_OVERLAY_29_02313814_H
--- src/overlay_29_02313814.c
+++ src/overlay_29_02313814.c
@@ -253,3 +253,24 @@ void BoostDefensiveStat(struct entity *user, struct entity *target, struct StatI
     }
     UpdateStatusIconFlags(target);
 }
+
+enum flash_fire_status FlashFireShouldActivate(struct entity *attacker, struct entity *defender)
+{
+    if (!EntityIsValid__023118B4(defender))
+        return FLASH_FIRE_STATUS_NONE;
+
+    if (AbilityIsActiveVeneer(attacker, ABILITY_NORMALIZE))
+        return FLASH_FIRE_STATUS_NONE;
+
+#ifdef JAPAN
+    if (!DefenderAbilityIsActive__02311B94(attacker, defender, ABILITY_FLASH_FIRE))
+#else
+    if (!DefenderAbilityIsActive__02311B94(attacker, defender, ABILITY_FLASH_FIRE, TRUE))
+#endif
+        return FLASH_FIRE_STATUS_NONE;
+
+    if (GetEntInfo(defender)->stat_modifiers.flash_fire_boost >= 2)
+        return FLASH_FIRE_STATUS_MAXED;
+
+    return FLASH_FIRE_STATUS_NOT_MAXED;
+}
