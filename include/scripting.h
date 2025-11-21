ff3e2fbf0dd19b5e4fe41d9d70a7ff41c22f48a9
slaw-22 <>
2025-11-21 19:08:12 +0000

    Decomp CompareScriptVariables and make CalcScriptVariables consistent

--- include/scripting.h
+++ include/scripting.h
@@ -42,5 +42,6 @@ void LoadScriptVariableValueString(const enum script_var_id sv_id, u8* result, u
 void SaveScriptVariableValueBytes(const enum script_var_id sv_id, u8* result, s32 num_bytes);
 s32 ScriptVariablesEqual(union script_var_value sv_val_ptr_local[], enum script_var_id sv_id_1, enum script_var_id sv_id_2);
 s32 CalcScriptVariables(s32 param_1, s32 param_2, enum script_calc_operation operation);
+bool8 CompareScriptVariables(s32 param_1, s32 param_2, enum compare_operation operation);
 
 #endif //PMDSKY_SCRIPTING_H
