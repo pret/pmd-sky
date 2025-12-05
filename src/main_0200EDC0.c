3ffd200f898c36730c3868deb1bb51f8be72bc1e
slaw-22 <slaw-22>
2025-12-05 17:50:07 +0000

    Decomp PerformanceProgressFlag funcs

--- src/main_0200EDC0.c
+++ src/main_0200EDC0.c
@@ -2,19 +2,18 @@
 #include "item.h"
 #include "main_0204CB94.h"
 #include "main_020517D4.h"
+#include "performance_progress.h"
 #include "script_variable.h"
 #include "special_episode.h"
 
 extern struct bag_items *BAG_ITEMS_PTR_MIRROR;
-
-extern bool8 GetPerformanceFlagWithChecks(s32 flag_id);
 
 s32 GetCurrentBagCapacity()
 {
     s32 special_episode_type = GetExecuteSpecialEpisodeType();
     if (special_episode_type == EPISODE_NONE)
     {
-        if (GetPerformanceFlagWithChecks(2) == 0)
+        if (GetResolvedPerformanceProgressFlag(2) == 0)
             return 1;
 
         return GetBagCapacity(GetScenarioBalance());
