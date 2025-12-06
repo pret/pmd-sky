06a96e6cc71f64cb6778c3d1e55a7aa6c7f91c70
slaw-22 <>
2025-12-06 07:16:53 +0000

    Further PR fixes

--- include/performance_progress.h
+++ include/performance_progress.h
@@ -1,7 +1,7 @@
 #ifndef PMDSKY_PERFORMANCE_PROGRESS_H
 #define PMDSKY_PERFORMANCE_PROGRESS_H
 
-u8 GetResolvedPerformanceProgressFlag(u32 idx);
+bool8 GetResolvedPerformanceProgressFlag(u32 idx);
 void SetResolvedPerformanceProgressFlag(u32 idx, s32 value);
 
 #endif //PMDSKY_PERFORMANCE_PROGRESS_H
