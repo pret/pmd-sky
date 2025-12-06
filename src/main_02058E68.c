5d95ef4340d95400ac351ddb127208a62a69949d
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-12-06 14:34:25 -0500

    Decomped EnableIqSkill and DisableIqSkill
    Decomped with help from @taxicat1.

--- src/main_02058E68.c
+++ src/main_02058E68.c
@@ -1,6 +1,34 @@
 #include "main_02058E68.h"
 #include "iq_skills.h"
 #include "main_02052B28.h"
+#include "monster_parameters.h"
+#include "pokemon.h"
+
+void DisableIqSkill(u32 *iq_skills_flags, s32 iq_id)
+{
+    if (IqSkillFlagTest(iq_skills_flags, iq_id))
+    {
+        s32 flags_group_index = iq_id / 32;
+        iq_skills_flags[flags_group_index] &= ~(1 << iq_id - (flags_group_index << 5));
+    }
+    else
+        EnableIqSkill(iq_skills_flags, iq_id);
+}
+
+void EnableIqSkill(u32 *iq_skills_flags, u32 iq_id)
+{
+    for (s32 id = 0; id < NUM_IQ_SKILLS; id++)
+    {
+        if (IQ_SKILL_RESTRICTIONS[iq_id] == IQ_SKILL_RESTRICTIONS[id])
+        {
+            s32 flags_group_index = id / 32;
+            iq_skills_flags[flags_group_index] &= ~(1 << id - (flags_group_index * 32));
+        }
+    }
+
+    u32 flags_group_index = iq_id / 32;
+    iq_skills_flags[flags_group_index] |= 1 << iq_id - (flags_group_index * 32);
+}
 
 enum iq_skill_id GetSpeciesIqSkill(s16 monster_id, u8 index)
 {
