60aa4e56a4b1773bdfae9039d55adc6bd99ad360
Seth Barberee <seth.barberee@gmail.com>
2026-03-28 18:46:39 -0700

    overlay 03 work

--- include/overlay_03_0233CA80.h
+++ include/overlay_03_0233CA80.h
@@ -5,9 +5,17 @@ struct Overlay03Main
 {
     u32 unk0;
     u32 unk4;
-    u8 fill8[0x10 - 8];
+    s32 unk8;
+    s32 unkC;
 };
 
 void ov03_0233CA80(void);
+void ov03_0233CA98(s32 arg0);
+void ov03_0233CAA8(s32 arg0);
+s32 ov03_0233CAB8(void);
+void ov03_0233CAC8(s32 arg0);
+s32 ov03_0233CAD8(void);
+void ov03_0233CAE8(s32 arg0);
+s32 ov03_0233CAF8(void);
 
 #endif //PMDSKY_OVERLAY_03_0233CA80_H
