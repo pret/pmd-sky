079f47d7ed95f942d0d4838eb47d7abbc6d0c9e3
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2026-04-26 23:10:14 -0400

    Decomped ov34_022DCB98
    Decomped by @JesusArcasCarrera

---
+++ src/overlay_34_022DCB98.c
@@ -0,0 +1,13 @@
+#include "overlay_34_022DCB98.h"
+#include "debug.h"
+
+extern void sub_02008F3C(s32, s32);
+
+void ov34_022DCB98(s32 arg0)
+{
+    if (Debug_GetDebugFlag(DEBUG_FLAG_NO_SCREEN_FADE) != 0) {
+        sub_02008F3C(1, 0);
+        return;
+    }
+    sub_02008F3C(1, arg0);
+}
