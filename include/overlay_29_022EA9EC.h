42b79c03f587b9796fe3113c87dae51856278164
Chesyon <55322011+Chesyon@users.noreply.github.com>
2026-05-06 08:18:22 -0400

    Define prng_state struct

--- include/overlay_29_022EA9EC.h
+++ include/overlay_29_022EA9EC.h
@@ -1,6 +1,16 @@
 #ifndef PMDSKY_OVERLAY_29_022EA9EC_H
 #define PMDSKY_OVERLAY_29_022EA9EC_H
 
+#include "util.h"
+
+typedef struct prng_state {
+    bool8 use_secondary;
+    u32 seq_num_primary;
+    u32 preseed;
+    u32 last_value_primary;
+    s32 idx_secondary;
+} prng_state;
+
 void InitDungeonRng(u32 seed);
 
 #endif //PMDSKY_OVERLAY_29_022EA9EC_H
--- src/overlay_29_022EA9EC.c
+++ src/overlay_29_022EA9EC.c
@@ -1,12 +1,12 @@
 #include "overlay_29_022EA9EC.h"
 
-extern u32 DUNGEON_PRNG_STATE[];
+extern prng_state DUNGEON_PRNG_STATE;
 extern u32 DUNGEON_PRNG_STATE_SECONDARY_VALUES[];
 
 void InitDungeonRng(u32 seed) {
-    DUNGEON_PRNG_STATE[3] = seed|1;
+    DUNGEON_PRNG_STATE.last_value_primary = seed|1;
     s32 scratch_0 = 0;
-    DUNGEON_PRNG_STATE[1] = scratch_0;
+    DUNGEON_PRNG_STATE.seq_num_primary = scratch_0;
     do {
         DUNGEON_PRNG_STATE_SECONDARY_VALUES[scratch_0]=seed;
         scratch_0++;
