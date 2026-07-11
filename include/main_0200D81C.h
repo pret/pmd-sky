539dd92880be980279829af4362fd0add0556583
Zur3l <90565581+Zur3l@users.noreply.github.com>
2026-07-11 16:00:08 -0400

    Decomp ItemZInit

---
+++ include/main_0200D81C.h
@@ -0,0 +1,8 @@
+#ifndef PMDSKY_MAIN_0200D81C_H
+#define PMDSKY_MAIN_0200D81C_H
+
+#include "item.h"
+
+void ItemZInit(struct item* item);
+
+#endif //PMDSKY_MAIN_0200D81C_H
---
+++ src/main_0200D81C.c
@@ -0,0 +1,9 @@
+#include "main_0200D81C.h"
+
+void ItemZInit(struct item* item)
+{
+    item->id = 0;
+    item->quantity = 0;
+    item->flags = 0;
+    item->held_by = 0;
+}
